5a9bd49cc242fe522b0c50a24177164279c41bac
limfiq <mtaufiq39@gmail.com>
2022-10-08 22:39:35 +0700

    Menambahkan Project State

---
+++ state/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
