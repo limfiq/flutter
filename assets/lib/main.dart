811b5d7eb6869b916746d3fa86f08f66324f15ca
limfiq <mtaufiq39@gmail.com>
2022-11-27 09:01:17 +0700

    27 nopember 2022

--- assets/lib/main.dart
+++ assets/lib/main.dart
@@ -16,39 +16,14 @@ class HomePage extends StatelessWidget {
         title: Text('Assets App'),
       ),
       body: Column(
-        children: [
-          Image.network(
-  String src, {
-  super.key,
-  double scale = 1.0,
-  this.frameBuilder,
-  this.loadingBuilder,
-  this.errorBuilder,
-  this.semanticLabel,
-  this.excludeFromSemantics = false,
-  this.width,
-  this.height,
-  this.color,
-  this.opacity,
-  this.colorBlendMode,
-  this.fit,
-  this.alignment = Alignment.center,
-  this.repeat = ImageRepeat.noRepeat,
-  this.centerSlice,
-  this.matchTextDirection = false,
-  this.gaplessPlayback = false,
-  this.filterQuality = FilterQuality.low,
-  this.isAntiAlias = false,
-  Map<String, String>? headers,
-  int? cacheWidth,
-  int? cacheHeight,
-}) : image = ResizeImage.resizeIfNeeded(cacheWidth, cacheHeight, NetworkImage(src, scale: scale, headers: headers)),
-     assert(alignment != null),
-     assert(repeat != null),
-     assert(matchTextDirection != null),
-     assert(cacheWidth == null || cacheWidth > 0),
-     assert(cacheHeight == null || cacheHeight > 0),
-     assert(isAntiAlias != null);],
+        children:[ 
+          CircleAvatar(
+            child: Image.asset('assets/image/pita.png'),
+          ),
+          CircleAvatar(
+            child: Image.network('https://en.wikipedia.org/wiki/User_%28computing%29#/media/File:User_icon_2.svg'),
+          )
+        ],
       ),
     );
   }
