80a71d4eefea2d56238302947436efc1e10bb07d
limfiq <mtaufiq39@gmail.com>
2022-12-02 13:20:58 +0700

    Menambahkan project navigasi

---
+++ navigasi/android/app/src/main/kotlin/com/example/navigasi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.navigasi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ navigasi/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Navigasi"),
+      ),
+      body: Container(
+        child: Drawer(),
+      ),
+    );
+  }
+}
