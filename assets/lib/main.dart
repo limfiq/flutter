61463954bdcffdc4f703d294b837d9880270fc04
limfiq <mtaufiq39@gmail.com>
2022-12-02 13:12:52 +0700

    update 2 Desember 2022

--- assets/lib/main.dart
+++ assets/lib/main.dart
@@ -13,12 +13,12 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey,
-      appBar: AppBar(
-        title: const Text('Halaman Utama'),
-      ),
-      body: Center(
-            child: Column(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: const Text('Halaman Utama'),
+        ),
+        body: Center(
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
@@ -34,19 +34,18 @@ class HomePage extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                 ),
               ),
-            
-        ElevatedButton(
-          child: const Text('Detail '),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const DetailPage()),
-            );
-          },
-        ),
-        ],
-      ),)
-    );
+              ElevatedButton(
+                child: const Text('Detail '),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const DetailPage()),
+                  );
+                },
+              ),
+            ],
+          ),
+        ));
   }
 }
 
@@ -56,94 +55,93 @@ class DetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green[200],
-      appBar: AppBar(
-        title: const Text('Halaman Detail'),
-      ),
-      body: Center(
-            child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage('assets/image/pm.png'),
-              ),
-              Text(
-                'Pesulap Merah',
-                style: TextStyle(
-                  fontFamily: 'Lobster',
-                  fontSize: 40.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                'Magician',
-                style: TextStyle(
-                  fontFamily: 'Josefin Sans',
-                  fontSize: 20.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 2.5,
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-                width: 150.0,
-                child: Divider(
-                  color: Colors.white,
-                ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(
-                  vertical: 10.0,
-                  horizontal: 25.0,
-                ),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.phone,
-                    color: Colors.indigo,
-                  ),
-                  title: Text(
-                    '+62 8888 8888 88',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      fontFamily: 'Josefin Sans',
-                      color: Colors.indigo,
-                    ),
-                  ),
-                ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(
-                  vertical: 10.0,
-                  horizontal: 25.0,
-                ),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.email,
-                    color: Colors.indigo,
-                  ),
-                  title: Text(
-                    'pesulapmerah@gmail.com',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      fontFamily: 'Josefin Sans',
-                      color: Colors.indigo,
-                    ),
-                  ),
-                ),
-              ),
-              ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('Go back!'),
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: const Text('Halaman Detail'),
         ),
-            ]
-      ),)
-    );
+        body: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('assets/image/pm.png'),
+                ),
+                Text(
+                  'Pesulap Merah',
+                  style: TextStyle(
+                    fontFamily: 'Lobster',
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'Magician',
+                  style: TextStyle(
+                    fontFamily: 'Josefin Sans',
+                    fontSize: 20.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.5,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.white,
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 25.0,
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.indigo,
+                    ),
+                    title: Text(
+                      '+62 8888 8888 88',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontFamily: 'Josefin Sans',
+                        color: Colors.indigo,
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 25.0,
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.indigo,
+                    ),
+                    title: Text(
+                      'pesulapmerah@gmail.com',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontFamily: 'Josefin Sans',
+                        color: Colors.indigo,
+                      ),
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Go back!'),
+                ),
+              ]),
+        ));
   }
 }
---
+++ assets/lib/main2.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Named Routes Demo',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const FirstScreen(),
+        '/second': (context) => const SecondScreen(),
+      },
+    ),
+  );
+}
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/second');
+          },
+          child: const Text('Launch screen'),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
