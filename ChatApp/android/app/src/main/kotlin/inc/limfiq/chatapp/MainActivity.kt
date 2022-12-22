df22da070dc40127b6f6a7a3de9afe6225c92e29
limfiq <mtaufiq39@gmail.com>
2022-12-22 22:54:05 +0700

    update malam jumat
    update malam jumat

---
+++ ChatApp/android/app/src/main/kotlin/inc/limfiq/chatapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package inc.limfiq.chatapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- dataflutter/lib/main.dart
+++ dataflutter/lib/main.dart
@@ -1,59 +1,26 @@
-import 'package:dataflutter/data.dart';
-import 'package:dataflutter/dataservice.dart';
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'home.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: DataPage(),
-  ));
-}
+void main() => runApp(App());
 
-class DataPage extends StatefulWidget {
-  const DataPage({super.key});
-
-  @override
-  State<DataPage> createState() => _DataPageState();
-}
-
-class _DataPageState extends State<DataPage> {
-  late Future tampil;
-  List<Data> tampilkan = [];
-  @override
-  void initState() {
-    tampil = DataService().getData();
-    tampil.then((value) {
-      setState(() {
-        tampilkan = value;
-      });
-    });
-    super.initState();
-  }
-
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'List Data',
-          style: TextStyle(color: Colors.black, fontSize: 25.0),
-        ),
-        backgroundColor: Colors.blue,
-        elevation: 0,
-      ),
-      body: tampilkan.length == 0
-          ? Center(
-              child: CircularProgressIndicator(
-                color: Colors.red,
-              ),
-            )
-          : ListView.builder(
-              itemCount: tampilkan.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(tampilkan[index].post_header),
-                  subtitle: Text(tampilkan[index].post_body),
-                );
-              }),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // themeMode: ThemeMode.dark,
+      theme: ThemeData(
+          primaryColor: Colors.black,
+          scaffoldBackgroundColor: Colors.blueGrey.shade900,
+          appBarTheme:
+              AppBarTheme(backgroundColor: Colors.transparent, elevation: 0)),
+      title: 'Flutter + PHP CRUD',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Home(),
+      },
     );
   }
 }
