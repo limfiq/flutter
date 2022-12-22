64dc8086850ab47f966858639311d1595a9b480e
limfiq <mtaufiq39@gmail.com>
2022-12-22 14:42:40 +0700

    Menampilkan data dari database

--- note_app/lib/main.dart
+++ note_app/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/noteService.dart';
+
+import 'package:note_app/note.dart';
 
 void main() {
   runApp(
@@ -16,6 +19,20 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late Future data;
+  List<Note> data2 = [];
+
+  @override
+  void initState() {
+    data = NoteService().getNote();
+    data.then((value) {
+      setState(() {
+        data2 = value;
+      });
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,25 +42,18 @@ class _HomePageState extends State<HomePage> {
             style: TextStyle(fontWeight: FontWeight.bold, fontFamily: "Roboto"),
           ),
         ),
-        body: ListView(
-          padding: const EdgeInsets.all(8),
-          children: <Widget>[
-            Container(
-              height: 50,
-              color: Colors.amber[600],
-              child: const Center(child: Text('Entry A')),
-            ),
-            Container(
-              height: 50,
-              color: Colors.amber[500],
-              child: const Center(child: Text('Entry B')),
-            ),
-            Container(
-              height: 50,
-              color: Colors.amber[100],
-              child: const Center(child: Text('Entry C')),
-            ),
-          ],
-        ));
+        body: data2.length == 0
+            ? Center(
+                child: CircularProgressIndicator(
+                  color: Colors.blue,
+                ),
+              )
+            : ListView.builder(
+                itemCount: data2.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(data2[index].title),
+                  );
+                }));
   }
 }
--- note_app/lib/noteService.dart
+++ note_app/lib/noteService.dart
@@ -1,3 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/note.dart';
 import 'package:http/http.dart' as http;
+
+class NoteService {
+  static final String _baseUrl = 'http://10.26.1.67/note/list.php';
+  Future getNote() async {
+    Uri uriApi = Uri.parse(_baseUrl);
+    final response = await http.get(uriApi);
+
+    if (response.statusCode == 200) {
+      return NoteFromJson(response.body.toString());
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+}
